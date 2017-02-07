937def04e87bebfbaaedcd2de3b8e70a4b6144e1
Russell Hallmark <russellhallmark@gmail.com>
2017-02-07 16:39:15 -0600

    CORS + Headers Issue 1

--- src/providers/authservice.ts
+++ src/providers/authservice.ts
@@ -7,8 +7,9 @@ export class AuthService {
     isLoggedin: boolean;
     AuthToken;
 
-    apiUrl: string = 'http://159.203.167.54';
-
+    //apiUrl: string = 'http://159.203.167.54';
+    apiUrl: string = 'http://hallmark.cloud';
+    
     constructor(public http: Http) {
         this.http = http;
         this.isLoggedin = false;
